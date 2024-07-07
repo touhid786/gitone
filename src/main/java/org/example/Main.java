5a9afc1767c8334831eeb3d18c3c466343a724d8
Touhid Sayed <sayyedtouheed704@gmail.com>
2024-07-07 17:29:12 +0530

    added a mul fun,done with the project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,15 @@ class Main{
     public static void main(String[] args) {
        Addition addition = new Addition();
        Subtraction subtraction = new Subtraction();
+        Multiplication multiplication = new Multiplication();
        int resultAdd = addition.add(50, 50);
        int resultSub = subtraction.subtract(100,50);
+       int reusltMul = multiplication.multiply(10,10);
        System.out.print(resultAdd);
         System.out.println();
        System.out.print(resultSub);
-
+        System.out.println();
+        System.out.println(reusltMul);
 
 
     }
---
+++ src/main/java/org/example/Multiplication.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Multiplication {
+    int multiply(int a, int b){
+        return a * b;
+    }
+}
